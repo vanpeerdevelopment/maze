5d421e97ea89a52366b2d21eea9e28b3e1a03950
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2024-05-25 11:55:42 +0200

    Use default express json body parser

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   const value = { hello: 'world' };
