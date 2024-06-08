ca49e9a9bcbfd1a1c6f15da14678e486580ac44f
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2024-06-08 17:23:53 +0200

    restart on post-deploy

--- pm2.config.js
+++ pm2.config.js
@@ -22,7 +22,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'https://github.com/vanpeerdevelopment/maze.git',
       path: '/home/nodejs/maze',
-      'post-deploy': 'pnpm install && pnpm build',
+      'post-deploy': 'pnpm install && pnpm build && pm2 startOrRestart pm2.config.json',
     },
   },
 };
